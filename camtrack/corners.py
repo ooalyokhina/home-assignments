723c5f7a32913e9f3055c21a99d87b49519f4a5a
alekhina503 <S0z2IVox>
2020-03-06 21:21:17 +0300

    fixed corners

--- camtrack/corners.py
+++ camtrack/corners.py
@@ -18,6 +18,10 @@ import pims
 from _corners import FrameCorners, CornerStorage, StorageImpl
 from _corners import dump, load, draw, without_short_tracks, create_cli
 
+maxCorners = 3000
+minDistance = 7
+max_diff = 0.2
+
 
 class _CornerStorageBuilder:
 
@@ -36,38 +40,61 @@ class _CornerStorageBuilder:
 
 def _build_impl(frame_sequence: pims.FramesSequence,
                 builder: _CornerStorageBuilder) -> None:
-    dist = 13
-    params = dict(maxCorners=1000,
-                  qualityLevel=0.01,
-                  minDistance=dist,
-                  useHarrisDetector=False,
-                  blockSize=dist)
-    prev = (frame_sequence[0] * 255).astype(np.uint8)
-    pts = cv2.goodFeaturesToTrack(image=prev, **params)
-    ids = np.arange(pts.shape[0])
-    max_id = len(ids)
-    builder.set_corners_at_frame(0, FrameCorners(ids, pts, np.full(len(pts), dist)))
-    for frame, cur in enumerate(frame_sequence[1:]):
-        cur = (cur * 255).astype(np.uint8)
-        pts, status, err = cv2.calcOpticalFlowPyrLK(prev, cur, pts, None, winSize=(dist, dist), minEigThreshold=0.002)
-        pts = pts[status.reshape(-1).astype(np.bool)]
-        ids = ids[status.reshape(-1).astype(np.bool)]
-        if len(pts) < 1000:
-            mask = get_mask(cur, pts, dist)
-            params['maxCorners'] = 1000 - len(pts)
-            pts = np.append(pts, cv2.goodFeaturesToTrack(cur, mask=mask, **params)).reshape((-1, 1, 2))
-            ids = np.append(ids, np.arange(max_id, max_id + len(pts)))
-            max_id = ids[len(ids) - 1] + 1
-            ids = np.arange(len(pts))
-        builder.set_corners_at_frame(frame + 1, FrameCorners(ids, pts, np.full(len(pts), dist)))
+    frame_sequence = list(map(lambda t: (np.array(t) * 255.0).astype(np.uint8), frame_sequence))
+    prev = frame_sequence[0]
+    points = cv2.goodFeaturesToTrack(prev,
+                                     maxCorners=maxCorners,
+                                     qualityLevel=0.05,
+                                     minDistance=minDistance,
+                                     blockSize=7).squeeze(axis=1)
+    ptr = len(points)
+    ids = np.arange(points)
+    sizes = np.full(ptr, 10)
+    builder.set_corners_at_frame(0, FrameCorners(ids, points, sizes))
+    idx = 0
+    for cur in frame_sequence[1:]:
+        idx += 1
+        dict(winSize=(15, 15),
+             maxLevel=2,
+             criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
+        fwd = cv2.calcOpticalFlowPyrLK(prev, cur,
+                                       points, None,
+                                       winSize=(15, 15), maxLevel=2,
+                                       criteria=(cv2.TERM_CRITERIA_EPS
+                                                 | cv2.TERM_CRITERIA_COUNT, 10, 0.03))[0].squeeze()
+        bwd = cv2.calcOpticalFlowPyrLK(cur, prev,
+                                       fwd, None,
+                                       winSize=(15, 15), maxLevel=2,
+                                       criteria=(cv2.TERM_CRITERIA_EPS
+                                                 | cv2.TERM_CRITERIA_COUNT, 10, 0.03))[0].squeeze()
+        mask = np.abs(points - bwd).max(-1) < max_diff
+        ids = ids[mask]
+        points = fwd[mask]
+        sizes = sizes[mask]
+        pts = points
+        if len(pts) < maxCorners:
+            next_features = cv2.goodFeaturesToTrack(cur,
+                                                    mask=get_mask(pts, cur),
+                                                    maxCorners=maxCorners,
+                                                    qualityLevel=0.05,
+                                                    minDistance=minDistance,
+                                                    blockSize=7)
+            next_features = next_features.squeeze(axis=1) if next_features is not None else []
+            for pnt in next_features[:maxCorners - len(pts)]:
+                ids = np.concatenate([ids, [ptr]])
+                points = np.concatenate([points, [pnt]])
+                sizes = np.concatenate([sizes, [10]])
+                ptr += 1
+
+        builder.set_corners_at_frame(idx, FrameCorners(ids, points, sizes))
         prev = cur
 
 
-def get_mask(cur, pts, dist):
-    mask = np.full_like(cur, 255)
-    for x, y in pts.reshape(-1, 2):
-        cv2.circle(mask, (x, y), dist, 0, -1)
-    return mask
+def get_mask(points_, shape):
+    mask = np.ones_like(shape, dtype=np.uint8)
+    for x, y in points_:
+        cv2.circle(mask, (x, y), minDistance, 0, -1)
+    return mask * 255
 
 
 def build(frame_sequence: pims.FramesSequence,
